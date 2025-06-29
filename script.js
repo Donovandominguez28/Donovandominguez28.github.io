5cb24bb6ec0309624d979cf190903b69a9104082
Donovandominguez28 <142244682+Donovandominguez28@users.noreply.github.com>
2025-06-29 16:56:36 -0600

    Mi sitio version 1

---
+++ script.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById("miCanvas");
+const ctx = canvas.getContext("2d");
+
+// Fondo
+ctx.fillStyle = "#f0f0f0";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// Texto
+ctx.fillStyle = "#333";
+ctx.font = "18px Arial";
+ctx.fillText("¡Hola desde Canvas!", 50, 40);
+
+// Dibujo simple
+ctx.fillStyle = "lightblue";
+ctx.beginPath();
+ctx.arc(150, 100, 40, 0, Math.PI * 2);
+ctx.fill();
+
+ctx.strokeStyle = "#000";
+ctx.lineWidth = 2;
+ctx.stroke();
